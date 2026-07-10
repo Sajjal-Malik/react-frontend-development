4edc228f88c53701ea13f3cf0b6df722a1324e4f
Sajjal Malik <sajjalmalik.sm@gmail.com>
2026-07-10 09:50:44 +0500

    useReducer Typing Code pushed

---
+++ 15-Typescript-with-React/09-ur-typing-practice/src/components/Counter.tsx
@@ -0,0 +1,27 @@
+import { type CounterState, counterReducer } from "../reducers/counterReducer";
+
+import { useReducer } from "react";
+
+const initialState: CounterState = { count: 0 };
+
+const Counter = () => {
+    const [state, dispatch] = useReducer(counterReducer, initialState);
+
+    const increment = () => {
+        dispatch({ type: "INCREMENT" });
+    };
+
+    const decrement = () => {
+        dispatch({ type: "DECREMENT" });
+    };
+
+    return (
+        <div>
+            <h2>Count: {state.count}</h2>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    );
+};
+
+export default Counter;
