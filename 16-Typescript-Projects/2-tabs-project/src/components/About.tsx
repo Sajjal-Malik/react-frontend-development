cad4bd0646156c6e502ec07026ac61652098a24b
Sajjal Malik <sajjalmalik.sm@gmail.com>
2026-07-23 09:21:19 +0500

    Tabs Project Added

---
+++ 16-Typescript-Projects/2-tabs-project/src/components/About.tsx
@@ -0,0 +1,24 @@
+const About = () => {
+    return (
+        <section className="bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+            <div className="max-w-4xl mx-auto">
+                <h2 className="text-3xl font-extrabold text-gray-900 mb-6">
+                    About Malik
+                </h2>
+                <p className="text-lg text-gray-700">
+                    Malik is a talented software engineer with a passion for creating
+                    stunning user experiences. Having worked with an extensive range of
+                    technologies, frameworks, and programming languages - from React and
+                    Angular to Python, Golang, and more.
+                </p>
+                <p className="mt-4 text-lg text-gray-700">
+                    However, He is still learning to take on new challenges and grow to become
+                    professional by trying out new things and taking those new challenges as opportunities
+                    instead of thinking them as hurdles.
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default About;
